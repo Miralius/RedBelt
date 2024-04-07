10ddbc86a607442029b8fece522ac76c4a5d295f
Антон Фролов <F-Miralius@yandex.ru>
2024-04-07 22:21:24 +0400

    Hotel book system task's solution

--- HotelBookSystem/main.cpp
+++ HotelBookSystem/main.cpp
@@ -13,13 +13,11 @@ using HotelName = string;
 using ClientId = uint32_t;
 using RoomCount = uint16_t;
 
-class HotelBookSystem
-{
+class HotelBookSystem {
     static constexpr int64_t historyDuration = 60 * 60 * 24;
     using RoomCountSum = uint64_t;
 public:
-    void Book(const TimePoint& time, const HotelName& hotelName, const ClientId& clientId, const RoomCount& roomCount)
-    {
+    void Book(const TimePoint& time, const HotelName& hotelName, const ClientId& clientId, const RoomCount& roomCount) {
         _timeRoomCountDequeMap[hotelName].emplace_back(time, roomCount);
         _roomCountMap[hotelName] += roomCount;
         _timeClientIdDequeMap[hotelName].emplace_back(time, clientId);
@@ -27,50 +25,41 @@ public:
         _lastTime = time;
     }
 
-    [[nodiscard]] uint32_t Clients(const HotelName& hotelName)
-    {
+    [[nodiscard]] uint32_t Clients(const HotelName& hotelName) {
+        adjustClients(hotelName);
         const auto hotelIt = _clientIdCounterDoubleMap.find(hotelName);
-        if (hotelIt != _clientIdCounterDoubleMap.cend())
-        {
-            adjustClients(hotelName);
+        if (hotelIt != _clientIdCounterDoubleMap.cend()) {
             return hotelIt->second.size();
         }
         return 0;
     }
 
-    [[nodiscard]] RoomCountSum Rooms(const HotelName& hotelName)
-    {
+    [[nodiscard]] RoomCountSum Rooms(const HotelName& hotelName) {
+        adjustRoomCount(hotelName);
         const auto hotelIt = _roomCountMap.find(hotelName);
-        if (hotelIt != _roomCountMap.cend())
-        {
-            adjustRoomCount(hotelName);
+        if (hotelIt != _roomCountMap.cend()) {
             return hotelIt->second;
         }
         return 0;
     }
 
 private:
-    void adjustRoomCount(const HotelName& hotelName)
-    {
+    void adjustRoomCount(const HotelName& hotelName) {
         auto& timeRoomCountDeque = _timeRoomCountDequeMap[hotelName];
         auto& roomCountSum = _roomCountMap[hotelName];
-        while (timeRoomCountDeque.front().first + historyDuration <= _lastTime)
-        {
+        while (!timeRoomCountDeque.empty() and timeRoomCountDeque.front().first + historyDuration <= _lastTime) {
             roomCountSum -= timeRoomCountDeque.front().second;
             timeRoomCountDeque.pop_front();
         }
     }
 
-    void adjustClients(const HotelName& hotelName)
-    {
+    void adjustClients(const HotelName& hotelName) {
         auto& timeClientIdDeque = _timeClientIdDequeMap[hotelName];
         auto& hotelClients = _clientIdCounterDoubleMap[hotelName];
-        while (timeClientIdDeque.front().first + historyDuration <= _lastTime)
-        {
+        while (!timeClientIdDeque.empty() and timeClientIdDeque.front().first + historyDuration <= _lastTime) {
             const auto frontClientId = timeClientIdDeque.front().second;
             auto clientIt = hotelClients.find(frontClientId);
-            if (--clientIt->second == 0)
-            {
+            if (--clientIt->second == 0) {
                 hotelClients.erase(clientIt);
             }
             timeClientIdDeque.pop_front();
@@ -96,33 +85,34 @@ int main() {
     int queryCount;
     cin >> queryCount;
 
-    for (int queryId = 0; queryId < queryCount; ++queryId) {
-        string queryType;
-        cin >> queryType;
+    try {
+        for (int queryId = 0; queryId < queryCount; ++queryId) {
+            string queryType;
+            cin >> queryType;
 
-        if (queryType == "BOOK") {
-            TimePoint time;
-            cin >> time;
-            HotelName hotelName;
-            cin >> hotelName;
-            ClientId clientId;
-            cin >> clientId;
-            RoomCount roomCount;
-            cin >> roomCount;
-            system.Book(time, hotelName, clientId, roomCount);
+            if (queryType == "BOOK") {
+                TimePoint time;
+                cin >> time;
+                HotelName hotelName;
+                cin >> hotelName;
+                ClientId clientId;
+                cin >> clientId;
+                RoomCount roomCount;
+                cin >> roomCount;
+                system.Book(time, hotelName, clientId, roomCount);
+            } else if (queryType == "CLIENTS") {
+                HotelName hotelName;
+                cin >> hotelName;
+                cout << system.Clients(hotelName) << '\n';
+            } else if (queryType == "ROOMS") {
+                HotelName hotelName;
+                cin >> hotelName;
+                cout << system.Rooms(hotelName) << '\n';
+            }
         }
-        else if (queryType == "CLIENTS")
-        {
-            HotelName hotelName;
-            cin >> hotelName;
-            cout << system.Clients(hotelName) << '\n';
-        }
-        else if (queryType == "ROOMS")
-        {
-            HotelName hotelName;
-            cin >> hotelName;
-            cout << system.Rooms(hotelName) << '\n';
-        }
+    }
+    catch (const exception& ex) {
+        cerr << "Exception! " << ex.what();
     }
 
     return 0;
