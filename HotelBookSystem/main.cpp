6766783d227c1d2653679e100b14db3eb7227fbf
Антон Фролов <F-Miralius@yandex.ru>
2024-04-07 21:01:02 +0400

    Hotel book system: implemented `Book`

--- HotelBookSystem/main.cpp
+++ HotelBookSystem/main.cpp
@@ -2,26 +2,62 @@
 // Created by F-Mir on 4/7/2024.
 //
 #include <iostream>
+#include <cstdint>
+#include <map>
+#include <deque>
 
 using namespace std;
-
-//time            int64_t
-//hotel_name      string
-//client_id       uint32_t
-//room_count      uint16_t
-//SUM(client_id)  uint32_t
-//SUM(room_count) uint64_t
-//
-//ROOMS   hotel_name → all(hotel.room_count) and time < 1 days
-//map<string, deque<pair<time, room_count>>> timeRoomCountDequeHotelMap
-//map<string, SUM(room_count)>               roomCountMap
-//CLIENTS hotel_name → all(hotel.clients) and time < 1 days
-//map<string, deque<pair<time, client_id>>> timeClientDequeHotelMap
-//map<string, set<client_id>>               clientSetHotelMap
+using TimePoint = int64_t;
+using HotelName = string;
+using ClientId = uint32_t;
+using RoomCount = uint16_t;
 
 class HotelBookSystem
 {
+    static constexpr int64_t historyDuration = 60 * 60 * 24;
+    using RoomCountSum = uint64_t;
+public:
+    void Book(const TimePoint& time, const HotelName& hotelName, const ClientId& clientId, const RoomCount& roomCount)
+    {
+        adjustRoomCount(time, hotelName, roomCount);
+        adjustClients(time, hotelName, clientId);
+    }
+private:
+    void adjustRoomCount(const TimePoint& time, const HotelName& hotelName, const RoomCount& roomCount)
+    {
+        auto& timeRoomCountDeque = _timeRoomCountDequeMap[hotelName];
+        timeRoomCountDeque.emplace_back(time, roomCount);
+        auto& roomCountSum = _roomCountMap[hotelName];
+        roomCountSum += roomCount;
+        while (timeRoomCountDeque.front().first + historyDuration <= time)
+        {
+            roomCountSum -= timeRoomCountDeque.front().second;
+            timeRoomCountDeque.pop_front();
+        }
+    }
 
+    void adjustClients(const TimePoint& time, const HotelName& hotelName, const ClientId& clientId)
+    {
+        auto& timeClientIdDeque = _timeClientIdDequeMap[hotelName];
+        timeClientIdDeque.emplace_back(time, clientId);
+        auto& hotelClients = _clientIdCounterDoubleMap[hotelName];
+        hotelClients[clientId]++;
+        while (timeClientIdDeque.front().first + historyDuration <= time)
+        {
+            const auto frontClientId = timeClientIdDeque.front().second;
+            auto clientIt = hotelClients.find(frontClientId);
+            if (--clientIt->second == 0)
+            {
+                hotelClients.erase(clientIt);
+            }
+            timeClientIdDeque.pop_front();
+        }
+    }
+
+    map<HotelName, deque<pair<TimePoint, RoomCount>>> _timeRoomCountDequeMap;
+    map<HotelName, RoomCountSum> _roomCountMap;
+    map<HotelName, deque<pair<TimePoint, ClientId>>> _timeClientIdDequeMap;
+    map<HotelName, map<ClientId, size_t /* counter */>> _clientIdCounterDoubleMap;
 };
 
 int main() {
@@ -31,23 +67,25 @@ int main() {
 //    ios::sync_with_stdio(false);
 //    cin.tie(nullptr);
 
-    [[maybe_unused]] HotelBookSystem manager;
+    HotelBookSystem system;
 
-    int query_count;
-    cin >> query_count;
+    int queryCount;
+    cin >> queryCount;
 
-    for (int query_id = 0; query_id < query_count; ++query_id) {
-        string query_type;
-        cin >> query_type;
-        int user_id;
-        cin >> user_id;
+    for (int queryId = 0; queryId < queryCount; ++queryId) {
+        string queryType;
+        cin >> queryType;
 
-        if (query_type == "READ") {
-            int page_count;
-            cin >> page_count;
-            //manager.Read(user_id, page_count);
-        } else if (query_type == "CHEER") {
-            //cout << setprecision(6) << manager.Cheer(user_id) << "\n";
+        if (queryType == "BOOK") {
+            TimePoint time;
+            cin >> time;
+            HotelName hotelName;
+            cin >> hotelName;
+            ClientId clientId;
+            cin >> clientId;
+            RoomCount roomCount;
+            cin >> roomCount;
+            system.Book(time, hotelName, clientId, roomCount);
         }
     }
 
