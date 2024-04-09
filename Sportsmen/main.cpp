4be26df47cac75bf09b403c1be9b2f936b830155
Антон Фролов <F-Miralius@yandex.ru>
2024-04-09 13:32:26 +0400

    Sportsmen solution

--- Sportsmen/main.cpp
+++ Sportsmen/main.cpp
@@ -1,8 +1,12 @@
 //
 // Created by F-Mir on 4/9/2024.
 //
-// The #if is needed for stepik check system submitting if we use the whole file
-#if __GNUC__ == 13 && __GNUC_MINOR__ == 2
+
+#if __GNUC__ != 13 || __GNUC_MINOR__ != 2
+#define STEPIK_CHECK_SYSTEM
+#endif
+
+#ifndef STEPIK_CHECK_SYSTEM
 
 #include "../TestRunner/TestRunner.h"
 #include "../Profiler/profile.h"
@@ -13,33 +17,90 @@
 #endif
 
 #include <list>
+#include <iostream>
+#include <random>
+#include <sstream>
+#include <set>
+#include <algorithm>
+
+template<class T>
+std::ostream& operator<<(std::ostream& os, const std::list<T>& s) {
+    for (const auto& x: s) {
+        os << x << ' ';
+    }
+    return os;
+}
 
 class Sportsmen {
 public:
+    void GetToLine(const unsigned who, const unsigned beforeWho) {
+        sportsmen.insert(std::find(sportsmen.cbegin(), sportsmen.cend(), beforeWho), who);
+    }
+
+    [[nodiscard]] std::list<unsigned> GetLine() const {
+        return sportsmen;
+    }
 
 private:
-
+    std::list<unsigned> sportsmen;
 };
 
-void TestWork()
-{
-    constexpr unsigned count = 5;
+void TestWork() {
+    Sportsmen sportsmen;
+    sportsmen.GetToLine(42, 0);
+    sportsmen.GetToLine(17, 42);
+    sportsmen.GetToLine(13, 0);
+    sportsmen.GetToLine(123, 42);
+    sportsmen.GetToLine(5, 13);
+    const std::list<unsigned> result = sportsmen.GetLine();
+    ASSERT_EQUAL(vector(result.cbegin(), result.cend()), vector({17u, 123u, 42u, 5u, 13u}))
 }
 
-void TestPerformance()
-{
-
+void TestPerformance() {
+    constexpr unsigned sportsmenCount = 100000;
+    std::random_device r;
+    std::default_random_engine e1(r());
+    std::uniform_int_distribution<int> uniform_dist(0, sportsmenCount);
+    std::set<unsigned> uniqueSportsmen;
+    std::vector<std::pair<unsigned, unsigned>> sportsmen;
+    sportsmen.reserve(sportsmenCount);
+    for (unsigned i = 0; i < sportsmenCount; i++) {
+        const auto& result = uniqueSportsmen.insert(uniform_dist(e1));
+        if (result.second) {
+            sportsmen.emplace_back(*result.first, uniform_dist(e1));
+        }
+    }
+    {
+        LOG_DURATION("Test performance")
+        Sportsmen sportsmenClass;
+        for (const auto& [who, beforeWho]: sportsmen) {
+            sportsmenClass.GetToLine(who, beforeWho);
+        }
+        std::ostringstream os;
+        os << sportsmenClass.GetLine();
+    }
 }
 
-void TestAll()
-{
+void TestAll() {
     TestRunner tr;
     RUN_TEST(tr, TestWork);
     RUN_TEST(tr, TestPerformance);
 }
 
-int main()
-{
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
     TestAll();
+    unsigned count;
+    std::cin >> count;
+    Sportsmen sportsmen;
+    for (unsigned i = 0; i < count; ++i) {
+        unsigned who;
+        std::cin >> who;
+        unsigned beforeWho;
+        std::cin >> beforeWho;
+        sportsmen.GetToLine(who, beforeWho);
+    }
+    std::cout << sportsmen.GetLine();
     return 0;
 }
