aa9eb6cd34bb9258c7813ca009a82b2f61f6dcd6
Антон Фролов <F-Miralius@yandex.ru>
2024-04-09 14:17:44 +0400

    Sportsmen optimized solution

--- Sportsmen/main.cpp
+++ Sportsmen/main.cpp
@@ -22,6 +22,8 @@
 #include <sstream>
 #include <set>
 #include <algorithm>
+#include <vector>
+#include <map>
 
 template<class T>
 std::ostream& operator<<(std::ostream& os, const std::list<T>& s) {
@@ -34,7 +36,17 @@ std::ostream& operator<<(std::ostream& os, const std::list<T>& s) {
 class Sportsmen {
 public:
     void GetToLine(const unsigned who, const unsigned beforeWho) {
-        sportsmen.insert(std::find(sportsmen.cbegin(), sportsmen.cend(), beforeWho), who);
+        const auto beforeWhoIt = sportsmenMap.find(beforeWho);
+        std::list<unsigned>::const_iterator newSportsmanIt;
+        if (beforeWhoIt != sportsmenMap.cend())
+        {
+            newSportsmanIt = sportsmen.insert(beforeWhoIt->second, who);
+        }
+        else
+        {
+            newSportsmanIt = sportsmen.insert(sportsmen.cend(), who);
+        }
+        sportsmenMap.insert({who, newSportsmanIt});
     }
 
     [[nodiscard]] std::list<unsigned> GetLine() const {
@@ -43,6 +55,7 @@ public:
 
 private:
     std::list<unsigned> sportsmen;
+    std::map<unsigned, std::list<unsigned>::const_iterator> sportsmenMap;
 };
 
 void TestWork() {
@@ -53,7 +66,7 @@ void TestWork() {
     sportsmen.GetToLine(123, 42);
     sportsmen.GetToLine(5, 13);
     const std::list<unsigned> result = sportsmen.GetLine();
-    ASSERT_EQUAL(vector(result.cbegin(), result.cend()), vector({17u, 123u, 42u, 5u, 13u}))
+    ASSERT_EQUAL(std::vector(result.cbegin(), result.cend()), std::vector({17u, 123u, 42u, 5u, 13u}))
 }
 
 void TestPerformance() {
